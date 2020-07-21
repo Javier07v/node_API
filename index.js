622d8fc2b85b3db0b385d63a987227fa77542df4
Javier Vallejos <javiervallejosaez@gmail.com>
2020-07-21 17:07:28 -0400

    commit 1

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('status!');
+});
+
+app.listen(3200, function () {
+  console.log('Example app listening on port 3000!');
+});
+
